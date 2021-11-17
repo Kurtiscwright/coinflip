8d38d230fe5f231dc37c5db3c390d4b473c08a8e
Kurtis <kurtiscwright@gmail.com>
2021-11-16 22:19:40 -0800

    added while loop, flip input option and pick a side option

--- flip.js
+++ flip.js
@@ -5,15 +5,18 @@ let tails = 1;
 let flip = Math.random();
 let result = 0;
  
-const flips = prompt('How many flips do you want to test');
-const side = prompt('Pick a number 0 for heads or 1 for tails');+
+const flips = prompt('How many flips do you want to test \n');
+const side = prompt('Pick a number 0 for heads or 1 for tails \n');
+
+//add start timer and timer + input time.
 
 while (result <= flips) {
     if (Math.round(flip) === side) {
         result++;
-        console.log('I found your side in ${result}');
+        return console.log('I found your side in ', result);
     } else {
         result++;
     }
 }
 
+return console.log('I did not find your side in ', result);
