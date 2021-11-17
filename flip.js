53d0c9968a3b3af59f30e121e84c381ff1ba3c4e
Kurtis Wright <Kurtiscwright@users.noreply.github.com>
2021-11-16 22:44:19 -0800

    Attempting to Add Timer

--- flip.js
+++ flip.js
@@ -9,6 +9,8 @@ const flips = prompt('How many flips do you want to test \n');
 const side = prompt('Pick a number 0 for heads or 1 for tails \n');
 
 //add start timer and timer + input time.
+let start = new Date;
+console.log('The current time is: ',start);
 
 while (result <= flips) {
     if (Math.round(flip) === side) {
